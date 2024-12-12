6304c73019d2155066b4e4b6ea0e9e0cfe3f94a5
7Rocky <rockygitt@gmail.com>
2024-12-12 01:14:03 +0000

    Tests

---
+++ day_11/main_test.rs
@@ -0,0 +1,82 @@
+use std::env;
+use std::fs;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+const ANSWER1: &str = "Stones after blinking 25 times (1): 235850";
+const ANSWER2: &str = "Stones after blinking 75 times (2): 279903140844645";
+
+const RESET: &str = "\x1b[0m";
+
+const RED_BACKGROUND: &str = "\x1b[41m";
+const GREEN_BACKGROUND: &str = "\x1b[42m";
+
+const RED_BOLD_BRIGHT: &str = "\x1b[1;91m";
+const GREEN_BOLD_BRIGHT: &str = "\x1b[1;92m";
+const YELLOW_BOLD_BRIGHT: &str = "\x1b[1;93m";
+const WHITE_BOLD_BRIGHT: &str = "\x1b[1;97m";
+
+fn print_pass_message() {
+    println!("{WHITE_BOLD_BRIGHT}{GREEN_BACKGROUND} PASS {RESET}");
+}
+
+fn print_fail_message() {
+    println!("{WHITE_BOLD_BRIGHT}{RED_BACKGROUND} FAIL {RESET}");
+}
+
+fn print_time(t: f64) {
+    println!(
+        "{WHITE_BOLD_BRIGHT} Time {} {t:.3} s{RESET}",
+        if t < 3.0 {
+            GREEN_BOLD_BRIGHT
+        } else if t < 20.0 {
+            YELLOW_BOLD_BRIGHT
+        } else {
+            RED_BOLD_BRIGHT
+        }
+    );
+}
+
+fn main() {
+    if env::args().len() == 2 && env::args().last().expect("First line must be initial time") == "time" {
+        println!(
+            "{}",
+            SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_micros()
+        );
+        return;
+    }
+
+    let output = fs::read_to_string("output.txt").expect("Should have been able to read the file");
+    let mut lines = output.trim().lines();
+
+    let init: u128 = lines
+        .next()
+        .expect("Should have a line here")
+        .parse()
+        .expect("Invalid integer");
+
+    let t = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_micros()
+        - init;
+    let mut pass = 0;
+
+    if lines.next().expect("Should have a line here") == ANSWER1 {
+        pass += 1;
+    }
+
+    if lines.next().expect("Should have a line here") == ANSWER2 {
+        pass += 1;
+    }
+
+    if pass == 2 {
+        print_pass_message();
+    } else {
+        print_fail_message();
+    }
+
+    print_time((t as f64) / 1000000.0);
+}
